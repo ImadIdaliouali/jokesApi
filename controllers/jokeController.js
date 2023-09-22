d7abc85fd0d6c3f16bf7034cd1d9f0cad14e7d18
imad.exe <imadidaliouali@gmail.com>
2023-09-22 17:16:52 +0100

    Implement joke controller functions

--- controllers/jokeController.js
+++ controllers/jokeController.js
@@ -1,13 +1,79 @@
 const Joke = require('../models/jokeModel');
 
-const getAllJokes = (req, res) => res.status(200).send('Get All Jokes');
+const getAllJokes = async (req, res) => {
+  try {
+    const jokes = await Joke.find();
 
-const getJoke = (req, res) => res.status(200).send('Get Joke');
+    res.status(200).json({
+      status: 'success',
+      results: jokes.length,
+      data: {
+        jokes,
+      },
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
 
-const createJoke = (req, res) => res.status(200).send('Create Joke');
+const getJoke = async (req, res) => {
+  try {
+    const joke = await Joke.findById(req.params.id);
 
-const updateJoke = (req, res) => res.status(200).send('Update Joke');
+    res.status(200).json({
+      status: 'success',
+      data: {
+        joke,
+      },
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
 
-const deleteJoke = (req, res) => res.status(200).send('Delete Joke');
+const createJoke = async (req, res) => {
+  try {
+    const { type, setup, punchline } = req.body;
+    const joke = await Joke.create({ type, setup, punchline });
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        joke,
+      },
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const updateJoke = async (req, res) => {
+  try {
+    const joke = await Joke.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        joke,
+      },
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const deleteJoke = async (req, res) => {
+  try {
+    await Joke.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({
+      status: 'success',
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 module.exports = { getAllJokes, getJoke, createJoke, updateJoke, deleteJoke };
