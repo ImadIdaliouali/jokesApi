865f4d944833e3d40f1f16ff9226df2b69e3eb4f
imad.exe <imadidaliouali@gmail.com>
2023-09-22 16:44:04 +0100

    Connect Express app to MongoDB

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
 
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB Connection: Successful'))
+  .catch((err) => console.error('MongoDB Connection Error:', err));
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
